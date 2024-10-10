803d1c97fe61086036cedea8d89ce1a94db3aede
gokul <gokulsunil2015@gmail.com>
2024-10-10 12:17:30 +0530

    Modify Admin Table

--- client/src/admin/AdminTable.jsx
+++ client/src/admin/AdminTable.jsx
@@ -2,63 +2,81 @@ import React, { useState, useEffect } from 'react';
 import Table from 'react-bootstrap/Table';
 import "../assets/css/table.css";
 import axios from 'axios';
+import Navbar from '../navbar/Navbar';
+import { DeleteOutlined } from '@ant-design/icons';
 
-function ReportTable() {
-  const [table, setTable] = useState([]);
 
-  const fetchData = async () => {
-    try {
-      const response = await axios.get(`http://localhost:8000/api/v1/user/getUsers`);
-      setTable(response.data.data);
-      
-    } catch (error) {
-      console.log(error);
+function AdminTable() {
+    const [questions, setQuestions] = useState([]);
+
+    const getQuestions = async () => {
+        try {
+            const response = await axios.get('http://localhost:8000/api/v1/question/getquestions')
+            console.log("admintable", response.data.data);
+            setQuestions(response.data.data);
+        } catch (error) {
+            console.log(error);
+        }
     }
-  };
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+    useEffect(() => {
+        getQuestions();
+    }, [])
 
-  return (
-    <div>
-      <Table striped>
-        <thead>
-          <tr>
-            <th>Sl.No</th>
-            <th>User Name</th>
-            <th>Email</th>
-            <th>Mark</th>
-            <th>Rating</th>
-            <th>Performance Category</th>
-            <th>Batch</th>
-            <th>Date</th>
+    const deleteQuestions = async (id) => {
+        try {
+            const response = await axios.delete(`http://localhost:8000/api/v1/question/deletequestions/${id}`);
+            console.log("deleted", response)
+            getQuestions();
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
 
-          </tr>
-        </thead>
-        <tbody>
-          {table.length > 0 ? (
-            table.map((item, index) => (
-              <tr key={item.id}> {/* Make sure to use a unique key */}
-                <td>{index + 1}</td>
-                <td>{item.fullName}</td>
-                <td>{item.email}</td>
-                <td>{item.score}</td>
-                <td>{item.performance}</td>
-                <td>{item.userStrength}</td>
-                <td>{item.batch}</td>
-                <td>{item.date}</td>
-              </tr>
-            ))
-          ) : (
-            <tr>
-              <td colSpan="6" className="text-center">No data available</td>
-            </tr>
-          )}
-        </tbody>
-      </Table>
-    </div>
-  );
+    return (
+        <div>
+            <Navbar />
+
+            <Table striped>
+                <thead>
+                    <tr>
+                        <th>Sl.No</th>
+                        <th>Question</th>
+                        <th>Option1</th>
+                        <th>Option2</th>
+                        <th>Option3</th>
+                        <th>Option4</th>
+                        <th>Answer</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {questions.length > 0 ? (
+                        questions.map((item, index) => (
+                            <tr key={item.id}>
+                                <td>{index + 1}</td>
+                                <td>{item.question}</td>
+                                <td>{item.option1}</td>
+                                <td>{item.option2}</td>
+                                <td>{item.option3}</td>
+                                <td>{item.option4}</td>
+                                <td>{item.correctAns}</td>
+                                <td>
+                                    <button className="btn btn-danger btn-sm" onClick={() => deleteQuestions(item._id)}><DeleteOutlined /></button>
+                                </td>
+                            </tr>
+                        ))
+                    ) : (
+                        <tr>
+                            <td colSpan="6" className="text-center">No data available</td>
+                        </tr>
+                    )}
+                </tbody>
+            </Table>
+
+        </div>
+    )
 }
 
-export default ReportTable;
+export default AdminTable
